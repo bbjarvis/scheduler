b17c7f4adb419d96df47a63152f011d57ca407da
bbjarvis <brettbjarvis@gmail.com>
2021-11-11 16:35:12 -0800

    add button functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+  if (props.danger) {
+     buttonClass += " button--danger"
+  }
+
+  return (
+   <button
+      disabled={props.disabled}
+      onClick={props.onClick}
+      className={buttonClass}
+   >
+      {props.children}
+   </button>
+   );
 }
