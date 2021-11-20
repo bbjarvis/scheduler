886b525dfa3fbd6f48309939e1e989613ca62f4a
bbjarvis <brettbjarvis@gmail.com>
2021-11-19 18:12:59 -0800

    add Show.js and it to stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 
 storiesOf("Button", module)
@@ -147,4 +148,5 @@ storiesOf("Button", module)
       .add("Appointment", () => <Appointment />)
       .add("Appointment with Time", () => <Appointment time="12pm" />)
       .add("Header", () => <Header time="12pm" />)
-      .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
+      .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+      .add("Show", () => <Show />);
