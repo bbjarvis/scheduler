8f36eb5194dfac08a333bcb3ab65bbc10e5ac722
bbjarvis <brettbjarvis@gmail.com>
2021-11-19 18:42:29 -0800

    add Error.js and its stories

---
+++ src/components/Appointment/Error.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from "classnames";
+import { action } from "@storybook/addon-actions/dist/preview";
+
+
+export default function Status(props) {
+  
+ return (
+<main className="appointment__card appointment__card--error">
+  <section className="appointment__error-message">
+    <h1 className="text--semi-bold">Error</h1>
+    <h3 className="text--light">{props.message}</h3>
+  </section>
+  <img
+    className="appointment__error-close"
+    src="images/close.png"
+    alt="Close"
+    onClick={props.onClose}
+  />
+</main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
 
 
 storiesOf("Button", module)
@@ -164,4 +165,9 @@ storiesOf("Button", module)
             onConfirm={action("onConfirm")}
             onCancel={action("onCancel")}
           />))
-       .add("Status", () => <Status message="Deleting"/>);
+      .add("Status", () => <Status message="Deleting"/>)
+      .add("Error", () => (
+        <Error
+          message="Could not delete appointment"
+          onClose={action("onClose")}
+        />));
